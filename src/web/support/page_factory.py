bc3fca6b0801cd3604aed97f549e9f4a248b76ce
Oleh Kuzovkov <kuzovkov.oleg@gmail.com>
2017-12-21 12:25:56 -0500

    Added PageFactory class storage.

--- src/web/support/page_factory.py
+++ src/web/support/page_factory.py
@@ -1,6 +1,17 @@
-def locator(by, value):
+class PageFactory:
+
     instances = {}
 
+    @classmethod
+    def get_instance(cls, key):
+        return cls.instances[key]
+
+    @classmethod
+    def set_instance(cls, key, page_object):
+        cls.instances[key] = page_object
+
+
+def locator(by, value):
     def real_decorator(class_):
         class WrapperMeta(type):
             def __getattr__(self, attr):
@@ -9,10 +20,10 @@ def locator(by, value):
         class Wrapper(metaclass=WrapperMeta):
             def __new__(cls, *args, **kwargs):
                 key = (class_, args, str(kwargs))
-                if key not in instances:
-                    page_ob = class_(*args, **kwargs)
-                    page_ob._locator = (by, value)
-                    instances[key] = page_ob
-                return instances[key]
+                if key not in PageFactory.instances:
+                    page_object = class_(*args, **kwargs)
+                    page_object._locator = (by, value)
+                    PageFactory.set_instance(key, page_object)
+                return PageFactory.get_instance(key)
         return Wrapper
     return real_decorator

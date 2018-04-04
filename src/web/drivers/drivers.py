8c68923051edbaea9c38f7d71d2ad1a6c1764f6b
Oleh Kuzovkov <kuzovkov.oleg@gmail.com>
2018-04-04 15:52:02 -0400

    [FIX] Fixed extension handling on driver's session creation.

--- src/web/drivers/drivers.py
+++ src/web/drivers/drivers.py
@@ -14,26 +14,27 @@ class Driver:
 
     hub_link = 'http://%s:%s/wd/hub' % (hub_ip, hub_port)
 
+    @staticmethod
+    def get_driver_type(browser_name):
+        if browser_name == 'firefox':
+            return FireFox
+        elif browser_name == 'internet explorer':
+            return InternetExplorer
+        else:
+            return Chrome
+
     def get_remove_session(self):
-        if self.extensions:
-            for extension in self.extensions:
-                Chrome.add_extension(extension)
+        driver_type = self.get_driver_type(capabilities['browserName'])
+        driver_capabilities = {**driver_type.get_capabilities(self.extensions), **capabilities}
         driver = webdriver.Remote(
             command_executor=self.hub_link,
-            desired_capabilities=capabilities)
+            desired_capabilities=driver_capabilities)
         return driver
 
     def get_local_session(self):
-        if capabilities['browserName'] == 'firefox':
-            driver_type = FireFox
-        elif capabilities['browserName'] == 'internet explorer':
-            driver_type = InternetExplorer
-        else:
-            driver_type = Chrome
-        if self.extensions:
-            for extension in self.extensions:
-                driver_type.add_extension(extension)
-        return driver_type.get_session()
+        driver_type = self.get_driver_type(capabilities['browserName'])
+        driver_capabilities = {**driver_type.get_capabilities(self.extensions), **capabilities}
+        return driver_type.get_session(driver_capabilities)
 
     def create_session(self):
         if remote_execution:
@@ -50,9 +51,12 @@ class Driver:
 class Chrome(Driver):
 
     @classmethod
-    def get_chrome_capabilities(cls):
+    def get_capabilities(cls, extensions=None):
         from selenium.webdriver.chrome.webdriver import Options as ChromeOptions
         chrome_options = ChromeOptions()
+        if extensions:
+            for extension in extensions:
+                chrome_options.add_extension(extension)
         chrome_options.add_experimental_option('prefs', {
             'credentials_enable_service': False,
             'profile': {
@@ -64,27 +68,17 @@ class Chrome(Driver):
         return chrome_capabilities
 
     @classmethod
-    def get_session(cls):
+    def get_session(cls, driver_capabilities):
         from webdriver_manager.chrome import ChromeDriverManager
-        driver_capabilities = {**cls.get_chrome_capabilities(), **capabilities}
         driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(),
                                   desired_capabilities=driver_capabilities)
         return driver
-
-    @staticmethod
-    def add_extension(extension_path):
-        file_ = open(extension_path, 'rb')
-        # Should not use base64.encodestring() which inserts newlines every
-        # 76 characters (per RFC 1521).  Chromedriver has to remove those
-        # unnecessary newlines before decoding, causing performance hit.
-        capabilities["goog:chromeOptions"]['extensions'] = [base64.b64encode(file_.read()).decode('UTF-8')]
-        file_.close()
 
 
 class FireFox(Driver):
 
     @classmethod
-    def get_firefox_capabilities(cls):
+    def get_capabilities(cls, extensions=None):
         firefox_capabilities = DesiredCapabilities.FIREFOX.copy()
         firefox_capabilities['loggingPrefs'] = {'browser': 'ALL'}
         return firefox_capabilities
@@ -92,7 +86,7 @@ class FireFox(Driver):
     @classmethod
     def get_session(cls):
         from webdriver_manager.firefox import GeckoDriverManager
-        driver_capabilities = {**cls.get_firefox_capabilities(), **capabilities}
+        driver_capabilities = {**cls.get_capabilities(), **capabilities}
         driver_capabilities.pop('browserSize')
         driver_capabilities.pop('version')
         driver_capabilities.pop('platform')
@@ -106,14 +100,14 @@ class FireFox(Driver):
 class InternetExplorer(Driver):
 
     @classmethod
-    def get_ie_capabilities(cls):
+    def get_capabilities(cls, extensions=None):
         return DesiredCapabilities.INTERNETEXPLORER.copy()
 
     @classmethod
     def get_session(cls):
         from webdriver_manager.microsoft import IEDriverManager
         capabilities.pop('platform')
-        driver_capabilities = {**cls.get_ie_capabilities(), **capabilities}
+        driver_capabilities = {**cls.get_capabilities(), **capabilities}
         driver = webdriver.Ie(executable_path=IEDriverManager(os_type="win32").install(),
                               capabilities=driver_capabilities)
         return driver
